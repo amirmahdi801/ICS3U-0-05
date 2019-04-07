e813e09f9f373d24a2548d2edf0193cbf79e6276
amirmahdi801 <40166909+amirmahdi801@users.noreply.github.com>
2019-04-07 20:13:43 +0430

    fix: added floor division

--- ICS5U-0-05.py
+++ ICS5U-0-05.py
@@ -1,12 +1,13 @@
 print ("Hi, in this program you can do basic mathematics")
 first = input("what your first number: ")
 second = input("what your  number: ")
-operation = input("Please choose: 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.power: ")
+operation = input("Please choose: 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.power 6.floor division: ")
 switcher = {
     "1": first + second,
     "2": int(first) - int(second),
     "3": int(first) * int(second),
     "4": int(first) / int(second),
-    "5": pow(first.second)
+    "5": int(first) ** int(second),
+    "6": int(first) // int(second)
 }
 print (switcher.get(operation))
