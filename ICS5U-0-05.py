cd29795d46d2776cfce56df9cdea062fd57795b7
amirmahdi801 <40166909+amirmahdi801@users.noreply.github.com>
2019-04-07 21:15:11 +0430

    fix: added comments

--- ICS5U-0-05.py
+++ ICS5U-0-05.py
@@ -1,13 +1,26 @@
 print ("Hi, in this program you can do basic mathematics")
-first = input("what your first number: ")
-second = input("what your  number: ")
-operation = input("Please choose: 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.power 6.floor division: ")
+first = input("what your first number: ") # first number
+second = input("what your  number: ") # second number
+operation = input("Please choose: 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.power 6.floor division\: ") #choices
+#the operations
 switcher = {
-    "1": first + second,
-    "2": int(first) - int(second),
-    "3": int(first) * int(second),
-    "4": int(first) / int(second),
-    "5": int(first) ** int(second),
-    "6": int(first) // int(second)
+    "1": first + second, #addition
+    "2": int(first) - int(second), #substraction
+    "3": int(first) * int(second), # multiplication
+    "4": int(first) / int(second), # division
+    "5": int(first) ** int(second), # power
+    "6": int(first) // int(second) #floor division
 }
-print (switcher.get(operation))
+print (switcher.get(operation)) #the output
+'''
+---------------------------------------
+-- Created by:     Amirmahdi Mersad  --
+-- Created on:     Apr 7 2019        --
+-- Created for:    Unit 0-05         --
+-- Course Code:    ICS3U             --
+-- Teacher Name:   Alireza TEimoori  --
+---------------------------------------
+-- this program is a calculator but  --
+it can also do power and floor division calculations--
+
+'''
